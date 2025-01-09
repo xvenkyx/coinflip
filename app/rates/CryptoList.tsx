905f334c6d79b3111efaa41b558c5588f8bb1ed8
xvenkyx <venkyroxx99@gmail.com>
2025-01-09 12:40:30 -0500

    added crypto-icons

--- app/rates/CryptoList.tsx
+++ app/rates/CryptoList.tsx
@@ -1,9 +1,12 @@
 // app/rates/CryptoList.tsx (Client Component)
 "use client";
 
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function CryptoList({ assets }: { assets: any[] }) {
+ 
+  const iconUrl = (symbol: string) =>
+    `/icons/color/${symbol.toLowerCase()}.svg`;
   return (
     <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
       {assets.map((asset) => (
@@ -13,8 +16,17 @@ export default function CryptoList({ assets }: { assets: any[] }) {
           className="p-4 border rounded-lg hover:shadow-lg transition"
         >
           <div>
+            <img
+              src={iconUrl(asset.symbol)}
+              alt={asset.name}
+              className="w-8 h-8 mb-2"
+              onError={(e) => {
+                (e.target as HTMLImageElement).src = "/icons/color/msr.svg"; // Fallback icon
+              }}
+            />
             <h2 className="text-lg font-bold">{asset.name}</h2>
             <p>Rank: {asset.rank}</p>
+            <p>Symbol: {asset.symbol}</p>
             <p>Price: ${parseFloat(asset.priceUsd).toFixed(2)}</p>
           </div>
         </Link>
